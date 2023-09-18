549c6a297e66086f0c7e534aa3fd5485b0bebf7f
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:49:51 +0200

    first commit

---
+++ src/main/java/it/fi/meucci/App.java
@@ -0,0 +1,15 @@
+package it.fi.meucci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String a="ciao";
+         System.out.println( a);
+        System.out.println( "Hello World!" );
+    }
+}
